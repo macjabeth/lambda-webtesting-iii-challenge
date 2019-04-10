112892c2a355d68d7f0837837a25c4e4282215b6
macjabeth <macbethjonathan@gmail.com>
2019-04-10 16:28:30 -0400

    implement unit test to check correct display of gate text and classes

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent, waitForElement } from 'react-testing-library';
+import 'react-testing-library/cleanup-after-each';
 
 import Dashboard from './Dashboard';
 
@@ -9,4 +11,25 @@ describe('<Dashboard />', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('displays correct gate text and classes', async () => {
+    const { getByText } = render(<Dashboard />);
+
+    const openPanel = await waitForElement(() => getByText('Open'));
+    expect(openPanel.textContent).toBe('Open');
+    expect(openPanel.classList).toContain('green-led');
+
+    const unlockedPanel = await waitForElement(() => getByText('Unlocked'));
+    expect(unlockedPanel.textContent).toBe('Unlocked');
+    expect(unlockedPanel.classList).toContain('green-led');
+
+    const closeBtn = await waitForElement(() => getByText('Close Gate'));
+    fireEvent.click(closeBtn);
+    expect(openPanel.textContent).toBe('Closed');
+    expect(openPanel.classList).toContain('red-led');
+
+    const lockBtn = await waitForElement(() => getByText('Lock Gate'));
+    fireEvent.click(lockBtn);
+    expect(unlockedPanel.textContent).toBe('Locked');
+    expect(unlockedPanel.classList).toContain('red-led');
+  });
 });
